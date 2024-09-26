d59c5d813b899983d42085cf0d9bd1936a8324a1
anapsgp <147257648+anapsgp@users.noreply.github.com>
2024-09-26 13:10:02 +0200

    ejercicio terminado

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,37 @@
-// El styles lo importamos aquí, ya se carga después al compilar todo
+// // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const passwordElement = document.getElementById('password');
+const passwordLengthElement = document.getElementById('password-length');
+// const rangeElement = document.getElementById('range')
+const buttonGenerateElement = document.getElementById('generate-password');
+
+const passwordSelectedRangeElement = document.getElementById('password-selected-range');
+
+passwordSelectedRangeElement.addEventListener('change', () => {
+  passwordLengthElement.innerText = passwordSelectedRangeElement.value;
+});
+
+// cuando el input cambie, cambia el valor de password-length
+
+const generateRandomNumber = max => {
+  return Math.floor(Math.random() * max);
+};
+
+const passwordCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()_+-={}[]:;<>,.?/';
+
+const generatePassword = () => {
+  let passwordLength = parseInt(passwordLengthElement.innerText);
+
+  let newPassword = '';
+
+  for (let i = 0; i < passwordLength; i++) {
+    let randomNumber = generateRandomNumber(passwordCharacters.length);
+    let randomChar = passwordCharacters.charAt(randomNumber);
+    newPassword += randomChar;
+  }
+
+  passwordElement.value = newPassword;
+};
+
+buttonGenerateElement.addEventListener('click', generatePassword);
